5ae0173b16404e52ad9ddd725a0ed89744b0e03d
llivol <adriapuigdellivol@gmail.com>
2021-04-30 18:12:11 +0200

    create project + main scene + button prefab

---
+++ number-selector/Assets/Scripts/NumberButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberButton : MonoBehaviour
+{
+    [SerializeField] private Button btnNumber;
+    [SerializeField] private Image imgBackground;
+    [SerializeField] private Text txtNumber;
+
+    private void Awake() 
+    {
+        btnNumber.onClick.AddListener(onClickNumber);
+    }
+}
