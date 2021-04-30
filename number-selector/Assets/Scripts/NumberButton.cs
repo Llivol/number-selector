4371e97dfff48c3b3c2f03b92a18d38503694232
llivol <adriapuigdellivol@gmail.com>
2021-04-30 19:24:50 +0200

    Show/Hide funcs in number button

--- number-selector/Assets/Scripts/NumberButton.cs
+++ number-selector/Assets/Scripts/NumberButton.cs
@@ -5,28 +5,65 @@ using UnityEngine.UI;
 
 public class NumberButton : MonoBehaviour
 {
+    private const float FADE_SECONDS = 2f;
+    
     [SerializeField] private Button btnNumber;
     [SerializeField] private Image imgBackground;
     [SerializeField] private Text txtNumber;
 
-    private void Awake() 
+    public void Show()
     {
-        btnNumber.onClick.AddListener(onClickNumber);
+        StartCoroutine(fadeIn(imgBackground));
+        StartCoroutine(fadeIn(txtNumber));
+        StartCoroutine(waitForSecondsAndEnable(FADE_SECONDS));
     }
 
-    private void onClickNumber()
+    public void Hide()
     {
         btnNumber.interactable = false;
         StartCoroutine(fadeOut(imgBackground));
         StartCoroutine(fadeOut(txtNumber));
     }
 
+    private void Awake() 
+    {
+        btnNumber.onClick.AddListener(onClickNumber);
+        Show();
+    }
+
+    private void onClickNumber()
+    {
+        Hide();
+    }
+
+    private IEnumerator fadeIn(Image image)
+    {
+        Color curColor = image.color;
+        while(curColor.a < 1f) 
+        {
+            curColor.a = curColor.a + (1 / FADE_SECONDS * Time.deltaTime);
+            image.color = curColor;
+            yield return null;
+        }
+    }
+
+    private IEnumerator fadeIn(Text text)
+    {
+        Color curColor = text.color;
+        while(curColor.a < 1f) 
+        {
+            curColor.a = curColor.a + (1 / FADE_SECONDS * Time.deltaTime);
+            text.color = curColor;
+            yield return null;
+        }
+    }
+
     private IEnumerator fadeOut(Image image)
     {
         Color curColor = image.color;
-        while(curColor.a > 0.0001f) 
+        while(curColor.a > 0f) 
         {
-            curColor.a = curColor.a - (.5f * Time.deltaTime);
+            curColor.a = curColor.a - (1 / FADE_SECONDS * Time.deltaTime);
             image.color = curColor;
             yield return null;
         }
@@ -35,12 +72,17 @@ public class NumberButton : MonoBehaviour
     private IEnumerator fadeOut(Text text)
     {
         Color curColor = text.color;
-        while(curColor.a > 0.0001f) 
+        while(curColor.a > 0f) 
         {
-            curColor.a = curColor.a - (.5f * Time.deltaTime);
+            curColor.a = curColor.a - (1 / FADE_SECONDS * Time.deltaTime);
             text.color = curColor;
             yield return null;
         }
     }
-
+    
+    private IEnumerator waitForSecondsAndEnable(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        btnNumber.interactable = true;
+    }
 }
