bca632412741a31aa3e0a973a316d9323caf5a90
llivol <adriapuigdellivol@gmail.com>
2021-04-30 18:52:41 +0200

    fadeIn/Out number button

--- number-selector/Assets/Scripts/NumberButton.cs
+++ number-selector/Assets/Scripts/NumberButton.cs
@@ -13,4 +13,34 @@ public class NumberButton : MonoBehaviour
     {
         btnNumber.onClick.AddListener(onClickNumber);
     }
+
+    private void onClickNumber()
+    {
+        btnNumber.interactable = false;
+        StartCoroutine(fadeOut(imgBackground));
+        StartCoroutine(fadeOut(txtNumber));
+    }
+
+    private IEnumerator fadeOut(Image image)
+    {
+        Color curColor = image.color;
+        while(curColor.a > 0.0001f) 
+        {
+            curColor.a = curColor.a - (.5f * Time.deltaTime);
+            image.color = curColor;
+            yield return null;
+        }
+    }
+
+    private IEnumerator fadeOut(Text text)
+    {
+        Color curColor = text.color;
+        while(curColor.a > 0.0001f) 
+        {
+            curColor.a = curColor.a - (.5f * Time.deltaTime);
+            text.color = curColor;
+            yield return null;
+        }
+    }
+
 }
